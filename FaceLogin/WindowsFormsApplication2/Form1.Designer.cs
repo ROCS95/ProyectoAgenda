ee53c66886f5f7d63aad6dfc9f99b91e229d678d
Robert Chacon Soto <chacon9595@hotmail.com>
2016-02-12 14:57:20 -0600

    login de facebook
    funcional (creo)

---
+++ FaceLogin/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wbFace = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // wbFace
+            // 
+            this.wbFace.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbFace.Location = new System.Drawing.Point(0, 0);
+            this.wbFace.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbFace.Name = "wbFace";
+            this.wbFace.Size = new System.Drawing.Size(594, 299);
+            this.wbFace.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 299);
+            this.Controls.Add(this.wbFace);
+            this.Name = "Form1";
+            this.Text = "Facebook Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wbFace;
+    }
+}
+
---
+++ FaceLogin/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using Facebook;
+using System.Dynamic;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const string appId = "804819252966453";
+        private Uri _loginUrl;
+        private const string _ExtendedPermissions = "user_about_me, publish_actions";
+        FacebookClient fbcliente = new FacebookClient();
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dynamic parameters = new ExpandoObject();
+            parameters.client_id = appId;
+            parameters.redirect_uri = "https://www.facebook.com/connect/login_success.html";
+            //parameters.redirect_url = "https://www.facebook.com/connect/login_success.html";
+            parameters.response_type = "token";
+            parameters.display = "popup";
+
+            if (!string.IsNullOrWhiteSpace(_ExtendedPermissions))
+            {
+                parameters.scope = _ExtendedPermissions;
+
+
+                var fb = new FacebookClient();
+                _loginUrl = fb.GetLoginUrl(parameters);
+
+                wbFace.Navigate(_loginUrl.AbsoluteUri);
+            }
+        }
+    }
+}
