895492e7096bc1ac7f668b407b0956c5fb0ec6d3
jose02942015 <chonio14@hotmail.com>
2016-02-16 10:24:35 -0600

    se agrego la interfas del proyecto

---
+++ JoseLedezmaAgendaSocial/JoseLedezmaAgendaSocial/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace JoseLedezmaAgendaSocial
+{
+    partial class Agenda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.pictBxMost = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAgreg = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBxMost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.Color.DarkGray;
+            this.monthCalendar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 25);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDown);
+            this.groupBox1.Controls.Add(this.monthCalendar1);
+            this.groupBox1.Location = new System.Drawing.Point(58, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(282, 217);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDown.Location = new System.Drawing.Point(207, 194);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 1;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictBxMost
+            // 
+            this.pictBxMost.Image = global::JoseLedezmaAgendaSocial.Properties.Resources.calendario;
+            this.pictBxMost.Location = new System.Drawing.Point(305, 59);
+            this.pictBxMost.Name = "pictBxMost";
+            this.pictBxMost.Size = new System.Drawing.Size(35, 30);
+            this.pictBxMost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictBxMost.TabIndex = 2;
+            this.pictBxMost.TabStop = false;
+            this.pictBxMost.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 59);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(378, 95);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(284, 169);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(443, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Agregar actividad";
+            this.label1.Visible = false;
+            // 
+            // btnAgreg
+            // 
+            this.btnAgreg.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgreg.Location = new System.Drawing.Point(484, 270);
+            this.btnAgreg.Name = "btnAgreg";
+            this.btnAgreg.Size = new System.Drawing.Size(82, 41);
+            this.btnAgreg.TabIndex = 6;
+            this.btnAgreg.Text = "Agregar";
+            this.btnAgreg.UseVisualStyleBackColor = true;
+            this.btnAgreg.Visible = false;
+            this.btnAgreg.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "BIENVENIDO";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(444, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 8;
+            // 
+            // Agenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 349);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAgreg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictBxMost);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Agenda";
+            this.Text = "Agenda Social";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictBxMost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.PictureBox pictBxMost;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgreg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ JoseLedezmaAgendaSocial/JoseLedezmaAgendaSocial/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JoseLedezmaAgendaSocial
+{
+    public partial class Agenda : Form
+    {
+        public Agenda()
+        {
+            InitializeComponent();
+            label2.Visible = true;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Show();
+            textBox2.Visible = true;
+            label1.Visible = true;
+            btnAgreg.Visible = true;
+            
+            label3.Text = DateTime.Now.ToString();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBox1.Text = monthCalendar1.SelectionStart.ToString("dd-MMMM-yyyy");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            textBox2.Visible = false;
+            label1.Visible = false;
+            btnAgreg.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream txt = new FileStream("C://Users//yessenia//Documents//Visual Studio 2015//Projects//JoseLedezmaAgendaSocial//Actividades.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(txt);
+            writer.WriteLine(textBox2.Text + " "+ monthCalendar1.SelectionStart.ToString("dd-MMMM-yyyy"));
+            writer.Close();
+            txt.Close();
+        }
+    }
+}
